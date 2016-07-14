0a086ee8fdb7dcd685f35eb5f5fa2c96c25f8528
Jason Mulvenna <jason.mulvenna@gmail.com>
2016-07-14 18:32:22 +1000

    Updating dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clj-reciprocal-blast "0.1.1"
+(defproject clj-reciprocal-blast "0.1.2"
   :description "Clojure library for running reciprocal blast"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-blast "0.2.5"]
+                 [clj-blast "0.2.6"]
                  [biodb "0.1.9"]
                  [clj-fasta "0.1.7"]
                  [me.raynes/fs "1.4.6"]])
